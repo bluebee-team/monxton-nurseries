18d83f696fe1ab5e786f1e0b7e9b1e27d154334c
bluebee-team <95026268+bluebee-team@users.noreply.github.com>
2025-04-10 15:50:22 +0100

    New plugin and code updates
    Custom code, mostly for product page
    Yith

--- app/public/wp-content/themes/monxton/functions.php
+++ app/public/wp-content/themes/monxton/functions.php
@@ -29,3 +29,35 @@ function input_to_button( $button, $form ) {
 
     return $dom->saveHtml( $new_button );
 }
+
+
+
+// Add tab to product page
+/**
+ * @snippet       New Product Tab @ WooCommerce Single Product
+ * @how-to        businessbloomer.com/woocommerce-customization
+ * @author        Rodolfo Melogli, Business Bloomer
+ * @compatible    WooCommerce 8
+ * @community     https://businessbloomer.com/club/
+ */
+ 
+add_filter( 'woocommerce_product_tabs', 'soil_add_product_tab', 9999 );
+   
+function soil_add_product_tab( $tabs ) {
+   $tabs['docs'] = array(
+      'title' => __( 'Soil Preferences', 'woocommerce' ), // TAB TITLE
+      'priority' => 50, // TAB SORTING (DESC 10, ADD INFO 20, REVIEWS 30)
+      'callback' => 'soil_add_product_tab_content', // TAB CONTENT CALLBACK
+   );
+   return $tabs;
+}
+ 
+function soil_add_product_tab_content() {
+   global $product;
+   echo '<h2>Soil Preferences</h2>';
+   if( get_field('soil_preferences')):
+    echo the_field('soil_preferences');
+endif;
+}
+
+
