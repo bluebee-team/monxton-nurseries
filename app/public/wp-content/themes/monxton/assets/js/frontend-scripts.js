3ae5529bdead0bca1b36877443f33a1766c5690e
bluebee-team <oliver@yanda.agency>
2025-04-17 16:14:01 +0100

    Bug fixes

--- app/public/wp-content/themes/monxton/assets/js/frontend-scripts.js
+++ app/public/wp-content/themes/monxton/assets/js/frontend-scripts.js
@@ -1,38 +1,45 @@
 
-// // Avoid Header
-function updatePadding() {
-    // Get the header element
-    const header = document.querySelector('header.wp-block-template-part');
+// // // Avoid Header
+// function updatePadding() {
+//     // Get the header element
+//     const header = document.querySelector('header.wp-block-template-part');
 
-    // Check if the header element exists
-    if (header) {
-        // Get the height of the header
-        const headerHeight = header.offsetHeight; // Add 60px to the header height
+//     // Check if the header element exists
+//     if (header) {
+//         // Get the height of the header
+//         const headerHeight = header.offsetHeight; // Add 60px to the header height
 
-        // Get all elements with the class .is-style-avoid-header
-        const elements = document.querySelectorAll('header.wp-block-template-part + .wp-block-group, header.wp-block-template-part + .wp-block-cover, .entry-content:not(.inner-content) > .wp-block-group:first-child, .entry-content:not(.inner-content) > .wp-block-cover:first-child:not(:has(> .wp-block-cover__inner-container > .wp-block-cover:first-child)), .entry-content:not(.inner-content) > .wp-block-cover:first-child > .wp-block-cover__inner-container > .wp-block-cover:first-child, .is-style-sticky');
+//         // Get all elements with the class .is-style-avoid-header
+//         const elements = document.querySelectorAll('header.wp-block-template-part + .wp-block-group, header.wp-block-template-part + .wp-block-cover, .entry-content:not(.inner-content) > .wp-block-group:first-child, .entry-content:not(.inner-content) > .wp-block-cover:first-child:not(:has(> .wp-block-cover__inner-container > .wp-block-cover:first-child)), .entry-content:not(.inner-content) > .wp-block-cover:first-child > .wp-block-cover__inner-container > .wp-block-cover:first-child, .is-style-sticky');
 
-        // Add padding to the top of each element equal to the header height
-        elements.forEach(function(element) {
-            element.style.paddingTop = headerHeight + 'px';
-        });
+//         // Add padding to the top of each element equal to the header height
+//         elements.forEach(function(element) {
+//             element.style.paddingTop = headerHeight + 'px';
+//         });
 
-    }
-}
+//     }
+// }
 
-window.addEventListener('load', function() {
-    updatePadding();
-});
+// window.addEventListener('load', function() {
+//     updatePadding();
+// });
 
-// Run Avoid Header on full page load
-document.addEventListener('DOMContentLoaded', function() {
-    updatePadding();
-});
+// // Run Avoid Header on full page load
+// document.addEventListener('DOMContentLoaded', function() {
+//     updatePadding();
+// });
 
-// Run Avoid Header on page resize
-window.addEventListener('resize', function() {
-    updatePadding();
-});
+// // Run Avoid Header on anchor link
+// document.addEventListener('hashchange', function() {
+//     alert('test');
+//     updatePadding();
+    
+// });
+
+// // Run Avoid Header on page resize
+// window.addEventListener('resize', function() {
+//     updatePadding();
+// });
 
 // Scrolling Header
 function scrollingHeader() {
